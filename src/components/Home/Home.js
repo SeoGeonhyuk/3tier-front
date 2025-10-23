d241c4863e08f8afac6fa5653938a231eb56db41
SmileShark <smileshark@SmileSharkui-MacBookAir.local>
2025-10-23 11:08:33 +0900

    feat: implementation show ip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,19 @@
 
-    import React, {Component} from 'react';
+    import React from 'react';
     import architecture from '../../assets/3TierArch.png'
 
-    class Home extends Component {
-        render () {
+    function Home() {
         const version = require('../../../package.json').version;
+        const instanceIp = window.location.hostname;
+
         return (
             <div>
             <h1 style={{color:"white"}}>AWS 3-TIER WEB APP DEMO</h1>
             <p style={{color:"#aaa", fontSize:"14px"}}>Frontend Version: {version}</p>
+            <p style={{color:"#aaa", fontSize:"14px"}}>Instance IP: {instanceIp}</p>
             <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
           </div>
         );
-      }
     }
 
     export default Home;
